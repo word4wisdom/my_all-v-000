5d044f16a985f19668fef70e34991685450672cb
Joan' Araujo <wisdombthegame@gmail.com>
2017-12-06 20:00:33 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,14 @@
 require 'pry'
 
-def
+def my_all?(collection)
 i = 0
+block_return_values = []
 while i < collection.length
-  i = i + 1
+block_return_values << yield(collection[i])
+  i += 1 
 end
 end
-binding.pry
+
+
+
+
